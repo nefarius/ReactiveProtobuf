bf16d51f0e81060eb0460d1b577027c2fd4b3050
Nefarius <nefarius@dhmx.at>
2014-12-23 00:58:22 +0100

    Finished deserialization

--- ReactiveProtobuf/Protocol/ProtobufChannel.cs
+++ ReactiveProtobuf/Protocol/ProtobufChannel.cs
@@ -17,19 +17,17 @@ namespace ReactiveProtobuf.Protocol
     public class ProtobufChannel<T> : IChannel<T>
     {
         private IReactiveSocket _socket;
-        private readonly Type _targetType;
         private readonly bool _isCompressed;
         private readonly bool _isEncrypted;
         private readonly SecureString _encKey;
 
-        public ProtobufChannel(IReactiveSocket socket, Type targetType, bool isCompressed)
-            : this(socket, targetType, isCompressed, false, null)
+        public ProtobufChannel(IReactiveSocket socket, bool isCompressed)
+            : this(socket, isCompressed, false, null)
         { }
 
-        public ProtobufChannel(IReactiveSocket socket, Type targetType, bool isCompressed = false, bool isEncrypted = false, SecureString encKey = null)
+        public ProtobufChannel(IReactiveSocket socket, bool isCompressed = false, bool isEncrypted = false, SecureString encKey = null)
         {
             _socket = socket;
-            _targetType = targetType;
             _isCompressed = isCompressed;
             _isEncrypted = isEncrypted;
             _encKey = encKey;
@@ -41,13 +39,6 @@ namespace ReactiveProtobuf.Protocol
                        select CreateObject(body.ToEnumerable().ToArray());
         }
 
-        public IObservable<object> Receiver { get; private set; }
-
-        public Task SendAsync(object message)
-        {
-            throw new NotImplementedException();
-        }
-
         private static byte[] GetBytes(string str)
         {
             byte[] bytes = new byte[str.Length * sizeof(char)];
@@ -55,7 +46,7 @@ namespace ReactiveProtobuf.Protocol
             return bytes;
         }
 
-        private object CreateObject(byte[] buffer)
+        private T CreateObject(byte[] buffer)
         {
             byte[] data = buffer;
 
@@ -71,19 +62,11 @@ namespace ReactiveProtobuf.Protocol
 
             using (var ms = new MemoryStream(data))
             {
-                MethodInfo method = typeof(Serializer).GetMethod("Deserialize");
-                MethodInfo generic = method.MakeGenericMethod(_targetType);
-                generic.Invoke(null, new object[] { ms });
-                Serializer.Deserialize<T>(ms);
+                return Serializer.Deserialize<T>(ms);
             }
-
-            return null;
         }
 
-        IObservable<T> IChannel<T>.Receiver
-        {
-            get { throw new NotImplementedException(); }
-        }
+        public IObservable<T> Receiver { get; private set; }
 
         public Task SendAsync(T message)
         {
